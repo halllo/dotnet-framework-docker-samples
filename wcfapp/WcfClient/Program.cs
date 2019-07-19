ee6d76abd130baf3c6bbced165ef1bbe6db0106e
Manuel Naujoks <mnaujoks@gmail.com>
2019-07-19 13:25:59 +0200

    updated dockerfiles and client stopwatch

--- wcfapp/WcfClient/Program.cs
+++ wcfapp/WcfClient/Program.cs
@@ -1,41 +1,48 @@
 ﻿using System;
+using System.Diagnostics;
 using System.ServiceModel;
 
 namespace WcfClient
 {
-    class Program
-    {
-        static string host;
+	class Program
+	{
+		static string host;
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Client OS: {0}", Environment.OSVersion);
-            host = Environment.GetEnvironmentVariable("host") ?? "localhost";
-            Console.WriteLine("Service Host: {0}", host);
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Client OS: {0}", Environment.OSVersion);
+			host = Environment.GetEnvironmentVariable("host") ?? "localhost";
+			Console.WriteLine("Service Host: {0}", host);
 
-            CallViaHttp();
+			CallViaHttp();
 
-            CallViaNetTcp();
-        }
+			CallViaNetTcp();
+		}
 
-        static void CallViaHttp()
-        {
-            var address = string.Format("http://{0}/Service1.svc", host);
-            var binding = new BasicHttpBinding();
-            var factory = new ChannelFactory<IService1>(binding, address);
-            var channel = factory.CreateChannel();
+		static void CallViaHttp()
+		{
+			var stopwatch = Stopwatch.StartNew();
+			var address = string.Format("http://{0}/Service1.svc", host);
+			var binding = new BasicHttpBinding();
+			var factory = new ChannelFactory<IService1>(binding, address);
+			var channel = factory.CreateChannel();
+			stopwatch.Stop();
 
-            Console.WriteLine(channel.Hello("WCF via Http"));
-        }
+			Console.WriteLine(channel.Hello("WCF via Http"));
+			Console.WriteLine($"took {stopwatch.ElapsedMilliseconds}ms");
+		}
 
-        static void CallViaNetTcp()
-        {
-            var address = string.Format("net.tcp://{0}/Service1.svc", host);
-            var binding = new NetTcpBinding(SecurityMode.None);
-            var factory = new ChannelFactory<IService1>(binding, address);
-            var channel = factory.CreateChannel();
+		static void CallViaNetTcp()
+		{
+			var stopwatch = Stopwatch.StartNew();
+			var address = string.Format("net.tcp://{0}/Service1.svc", host);
+			var binding = new NetTcpBinding(SecurityMode.None);
+			var factory = new ChannelFactory<IService1>(binding, address);
+			var channel = factory.CreateChannel();
+			stopwatch.Stop();
 
-            Console.WriteLine(channel.Hello("WCF via Net.Tcp"));
-        }
-    }
+			Console.WriteLine(channel.Hello("WCF via Net.Tcp"));
+			Console.WriteLine($"took {stopwatch.ElapsedMilliseconds}ms");
+		}
+	}
 }
